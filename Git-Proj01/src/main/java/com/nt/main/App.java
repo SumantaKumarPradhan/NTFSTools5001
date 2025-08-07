c2614458ee847334ced3f90089f5ca2195755d58
sumanta-demo <kumarsumanta083@gmail.com>
2025-08-08 00:24:08 +0530

    Proj01 created

---
+++ Git-Proj01/src/main/java/com/nt/main/App.java
@@ -0,0 +1,9 @@
+package com.nt.main;
+public class App {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("res : "+c);
+    }
+}
